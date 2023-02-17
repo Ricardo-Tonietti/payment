4c6c80700224d1211e9f1873297a2075dd860311
Ricardo Tonietti <rictonietti@gmail.com>
2023-02-17 14:10:37 -0300

    insert userCosumer(create queue) create first version 1.0.0

---
+++ src/main/java/com/ead/payment/consumers/UserConsumer.java
@@ -0,0 +1,22 @@
+package com.ead.payment.consumers;
+
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserConsumer {
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "${ead.broker.queue.userEventQueue.name}", durable = "true"),
+            exchange = @Exchange(value = "${ead.broker.exchange.userEventExchange}",
+                    type = ExchangeTypes.FANOUT, ignoreDeclarationExceptions = "true"))
+    )
+    public void listenUserEvent(@Payload String event){
+
+    }
+}
