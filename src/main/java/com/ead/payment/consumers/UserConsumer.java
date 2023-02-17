8cbec61066a1e88714167104acf7f779be2b16fb
Ricardo Tonietti <rictonietti@gmail.com>
2023-02-17 16:57:41 -0300

    implementation amqp

--- src/main/java/com/ead/payment/consumers/UserConsumer.java
+++ src/main/java/com/ead/payment/consumers/UserConsumer.java
@@ -1,22 +1,40 @@
 package com.ead.payment.consumers;
 
+import com.ead.payment.dtos.UserEventDto;
+import com.ead.payment.enums.ActionType;
+import com.ead.payment.services.UserService;
 import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserConsumer {
 
+    @Autowired
+    UserService userService;
+
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(value = "${ead.broker.queue.userEventQueue.name}", durable = "true"),
             exchange = @Exchange(value = "${ead.broker.exchange.userEventExchange}",
                     type = ExchangeTypes.FANOUT, ignoreDeclarationExceptions = "true"))
     )
-    public void listenUserEvent(@Payload String event){
+    public void listenUserEvent(@Payload UserEventDto userEventDto) {
+        var userModel = userEventDto.convertToUserModel();
 
+        switch (ActionType.valueOf(userEventDto.getActionType())) {
+            case CREATE:
+                userService.save(userModel);
+                break;
+            case UPDATE:
+                userService.save(userModel);
+                break;
+            case DELETE:
+                userService.delete(userEventDto.getUserId());
+        }
     }
 }
---
+++ src/main/java/com/ead/payment/services/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ead.payment.services.impl;
+
+import com.ead.payment.models.UserModel;
+import com.ead.payment.repositories.UserRepository;
+import com.ead.payment.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserModel save(UserModel userModel) {
+        return userRepository.save(userModel);
+    }
+
+   @Transactional
+    @Override
+    public void delete(UUID userId) {
+        userRepository.deleteById(userId);
+    }
+}
